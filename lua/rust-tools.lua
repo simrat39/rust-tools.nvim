4d258e85135591fb98deabc08ff3ed09201af770
simrat39 <simrats169169@gmail.com>
2021-04-01 22:02:59 -0700

    rust-tools: Add RustSetInlayHints command
    Does what it sounds like

--- lua/rust-tools.lua
+++ lua/rust-tools.lua
@@ -1,8 +1,9 @@
 local M = {}
 
 local function setupCommands()
-   vim.cmd("command! " .. "RustOpenCargo " .. ":lua require'rust-tools.open_cargo_toml'.open_cargo_toml()") 
+   vim.cmd("command! " .. "RustSetInlayHints " .. ":lua require'rust-tools.inlay_hints'.set_inlay_hints()") 
    vim.cmd("command! " .. "RustExpandMacro " .. ":lua require'rust-tools.expand_macro'.expand_macro()")
+   vim.cmd("command! " .. "RustOpenCargo " .. ":lua require'rust-tools.open_cargo_toml'.open_cargo_toml()") 
 end
 
 function M.setup()
