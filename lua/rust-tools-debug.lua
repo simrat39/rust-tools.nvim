e3eb59555402d3ad6f411e05de4485a386db4f39
simrat39 <simrats169169@gmail.com>
2021-04-03 13:48:58 -0700

    Add some commands for debugging
    needs plenary

---
+++ lua/rust-tools-debug.lua
@@ -0,0 +1,21 @@
+local vim = vim
+
+local D = {}
+
+-- needs plenary
+local reload = require('plenary.reload').reload_module
+
+function D.R(name)
+   reload(name)
+   return require(name)
+end
+
+local function setupCommands()
+   vim.cmd("command! " .. "DRustSetInlayHints " .. ":lua require'rust-tools-debug'.R('rust-tools.inlay_hints').set_inlay_hints()")
+end
+
+function D.setup()
+   setupCommands()
+end
+
+return D
