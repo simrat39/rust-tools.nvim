a39b004fe7852e97bafe1da89e42929f9746984a
simrat39 <simrats169169@gmail.com>
2021-04-05 18:05:56 -0700

    setup: Add option to disable auto inlay hints

--- lua/rust-tools.lua
+++ lua/rust-tools.lua
@@ -7,7 +7,7 @@ local function setupCommands()
    vim.cmd("command! " .. "RustExpandMacro " .. ":lua require'rust-tools.expand_macro'.expand_macro()")
    vim.cmd("command! " .. "RustOpenCargo " .. ":lua require'rust-tools.open_cargo_toml'.open_cargo_toml()")
    vim.cmd("command! " .. "RustParentModule " .. ":lua require'rust-tools.parent_module'.parent_module()")
-   vim.cmd("command! " .. "RustJoinLines " .. ":lua require'rust-tools.join_lines'.join_lines()")
+   vim.cmd("command! " .. "RustJoinLines " .. "parentmodu:lua require'rust-tools.join_lines'.join_lines()")
    vim.cmd("command! " .. "RustRunnables " .. ":lua require'rust-tools.runnables'.runnables()")
    vim.cmd("command! " .. "RustRunnablesTelescope " .. ":lua require('rust-tools.runnables').runnables_telescope(require('telescope.themes').get_dropdown({}))")
    vim.cmd("command! " .. "RustHoverActions " .. ":lua require'rust-tools.hover_actions'.hover_actions()")
@@ -15,8 +15,14 @@ local function setupCommands()
    vim.cmd("command! " .. "RustMoveItemUp " .. ":lua require'rust-tools.move_item'.move_item(true)")
 end
 
-function M.setup()
-    require'rust-tools.inlay_hints'.setup_autocmd()
+function M.setup(opts)
+    opts = opts or {}
+    if opts.autoSetHints == nil then opts.autoSetHints = true end
+
+    if opts.autoSetHints then
+        require'rust-tools.inlay_hints'.setup_autocmd()
+    end
+
     setupCommands()
 end
 
