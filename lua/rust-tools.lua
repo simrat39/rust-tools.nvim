ce7f9573df5d3018e2adc7dd1ef814e30f0b1917
Gabriel Sanches <gabriel@gsr.dev>
2021-04-06 22:42:30 -0300

    Make setup function be a wrapper for rust-analyzer
    This way, this plugin's functions only work when rust-analyzer is
    running via built-in LSP.

--- lua/rust-tools.lua
+++ lua/rust-tools.lua
@@ -1,55 +1,60 @@
-local vim = vim
-
 local M = {}
 
--- Takes a table and converts it into a long string
-local function table_to_long_str(t)
-   local ret = "{"
-   for key, value in pairs(t) do
-      ret = ret .. tostring(key) .. "="
-      -- recursively handle nested tables
-      if type(value) == 'table' then
-         ret = ret .. table_to_long_str(value) .. ","
-      else
-        ret = ret .. tostring(value) .. ","
-      end
+local function setup_commands(server_opts, tools_opts)
+   server_opts.commands = vim.tbl_extend('force', server_opts.commands or {}, {
+      RustSetInlayHints = {
+         function()
+            require('rust-tools.inlay_hints').set_inlay_hints(tools_opts.inlay_hints or {})
+         end,
+         -- TODO: Add description.
+      },
+      RustExpandMacro = {
+         require('rust-tools.expand_macro').expand_macro,
+         -- TODO: Add description.
+      },
+      RustOpenCargo = {
+         require('rust-tools.open_cargo_toml').open_cargo_toml,
+         -- TODO: Add description.
+      },
+      RustParentModule = {
+         require('rust-tools.parent_module').parent_module,
+         -- TODO: Add description.
+      },
+      RustJoinLines = {
+         require('rust-tools.join_lines').join_lines,
+         -- TODO: Add description.
+      },
+      RustRunnables = {
+         function()
+            require('rust-tools.runnables').runnables(tools_opts.runnables or {})
+         end,
+         -- TODO: Add description.
+      },
+      RustHoverActions = {
+         require('rust-tools.hover_actions').hover_actions
+         -- TODO: Add description.
+      },
+      RustMoveItemDown = {
+         require('rust-tools.move_item').move_item
+         -- TODO: Add description.
+      },
+      RustMoveItemUp = {
+         function()
+            require('rust-tools.move_item').move_item(true)
+         end,
+         -- TODO: Add description.
+      }
+   })
+end
+
+   function M.setup(opts)
+      opts = opts or {}
+      local server_opts = opts.server or {}
+      local tools_opts = opts.tools or {}
+
+      setup_commands(server_opts, tools_opts)
+
+      require('lspconfig').rust_analyzer.setup(server_opts)
    end
-   ret = ret .. "}"
-   return ret
-end
-
-local function setupCommands(opts)
-    local inlay_hints_opts = table_to_long_str(opts.inlay_hints or {})
-    vim.cmd("command! " .. "RustSetInlayHints " .. ":lua require'rust-tools.inlay_hints'.set_inlay_hints(" .. inlay_hints_opts .. ")")
-
-    vim.cmd("command! " .. "RustExpandMacro " .. ":lua require'rust-tools.expand_macro'.expand_macro()")
-    vim.cmd("command! " .. "RustOpenCargo " .. ":lua require'rust-tools.open_cargo_toml'.open_cargo_toml()")
-    vim.cmd("command! " .. "RustParentModule " .. ":lua require'rust-tools.parent_module'.parent_module()")
-    vim.cmd("command! " .. "RustJoinLines " .. ":lua require'rust-tools.join_lines'.join_lines()")
-
-    local runnable_opts = table_to_long_str(opts.runnables or {})
-    vim.cmd("command! " .. "RustRunnables " .. ":lua require'rust-tools.runnables'.runnables(" .. runnable_opts .. ")")
-    -- Setup the dropdown theme if telescope is installed
-    if pcall(require, 'telescope') then
-        vim.cmd("command! " .. "RustRunnables " .. ":lua require'rust-tools.runnables'.runnables(require('telescope.themes').get_dropdown(" .. runnable_opts .. "))")
-    end
-
-    vim.cmd("command! " .. "RustRunnablesTelescope " .. ":lua require('rust-tools.runnables').runnables_telescope(require('telescope.themes').get_dropdown({}))")
-    vim.cmd("command! " .. "RustHoverActions " .. ":lua require'rust-tools.hover_actions'.hover_actions()")
-    vim.cmd("command! " .. "RustMoveItemDown " .. ":lua require'rust-tools.move_item'.move_item()")
-    vim.cmd("command! " .. "RustMoveItemUp " .. ":lua require'rust-tools.move_item'.move_item(true)")
-end
-
-
-function M.setup(opts)
-    opts = opts or {}
-    if opts.autoSetHints == nil then opts.autoSetHints = true end
-
-    if opts.autoSetHints then
-        require'rust-tools.inlay_hints'.setup_autocmd(table_to_long_str(opts.inlay_hints or {}))
-    end
-
-    setupCommands(opts)
-end
 
 return M
