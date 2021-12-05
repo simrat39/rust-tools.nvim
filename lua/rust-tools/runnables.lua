4242f7388051caaa583878e19a6db4907b6cb201
Anshuman <amedhi@connect.ust.hk>
2021-12-05 12:55:09 +0800

    Remove dependency on telescope.nvim thanks to vim.ui.select

--- lua/rust-tools/runnables.lua
+++ lua/rust-tools/runnables.lua
@@ -59,58 +59,10 @@ local function handler(_, result)
     end)
 end
 
-local function get_telescope_handler(opts)
-	local pickers = require("telescope.pickers")
-	local finders = require("telescope.finders")
-	local sorters = require("telescope.sorters")
-	local actions = require("telescope.actions")
-	local action_state = require("telescope.actions.state")
-
-	return function(_, results)
-		local choices = getOptions(results, false, false)
-
-		local function attach_mappings(bufnr, map)
-			local function on_select()
-				local choice = action_state.get_selected_entry().index
-
-				actions.close(bufnr)
-				M.run_command(choice, results)
-			end
-
-			map("n", "<CR>", on_select)
-			map("i", "<CR>", on_select)
-
-			-- Additional mappings don't push the item to the tagstack.
-			return true
-		end
-
-		pickers.new(opts or {}, {
-			prompt_title = "Runnables",
-			finder = finders.new_table({ results = choices }),
-			sorter = sorters.get_generic_fuzzy_sorter(),
-			previewer = nil,
-			attach_mappings = attach_mappings,
-		}):find()
-	end
-end
-
 -- Sends the request to rust-analyzer to get the runnables and handles them
--- The opts provided here are forwarded to telescope, other than use_telescope
--- which is used to check whether we want to use telescope or the vanilla vim
--- way for input
+-- Uses vim.ui.select which defaults to vanilla vim but can be overriden to a telescope based searcher
 function M.runnables()
-	local opts = config.options.tools.runnables
-
-	-- this is the handler which is actually used, hence its the used handler
-	local used_handler = handler
-
-	-- if the user has both telescope installed and option set to use telescope
-	if pcall(require, "telescope") and opts.use_telescope then
-		used_handler = get_telescope_handler(opts)
-	end
-
-	-- fallback to the vanilla method incase telescope is not installed or the
-	-- user doesn't want to use it
+    local used_handler = handler or config.options.tools.runnables.select
 	utils.request(0, "experimental/runnables", get_params(), used_handler)
 end
 
