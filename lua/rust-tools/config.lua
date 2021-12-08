d67875ad3b3c78395f4c15d8f4d32c79923cc22f
William Boman <william@redwill.se>
2021-12-08 02:58:40 +0100

    chore: update link to lspconfig server config

--- lua/rust-tools/config.lua
+++ lua/rust-tools/config.lua
@@ -182,7 +182,7 @@ local defaults = {
 
 	-- all the opts to send to nvim-lspconfig
 	-- these override the defaults set by rust-tools.nvim
-	-- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+	-- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
 	server = {}, -- rust-analyer options
 
 	-- debugging stuff
