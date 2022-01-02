d3254401c9d3326b38ec6fdb82afcecbcdad6c1a
cht <3237126351@qq.com>
2022-01-02 15:48:16 +0800

    check if lsp_opts is nil

--- lua/rust-tools.lua
+++ lua/rust-tools.lua
@@ -173,7 +173,7 @@ function M.start_standalone_if_required()
 	local lsp_opts = config.options.server
 	local current_buf = vim.api.nvim_get_current_buf()
 
-	if lsp_opts.standalone and utils.is_bufnr_rust(current_buf) and (get_root_dir() == nil) then
+	if (lsp_opts ~=nil and lsp_opts.standalone) and utils.is_bufnr_rust(current_buf) and (get_root_dir() == nil) then
 		require("rust-tools.standalone").start_standalone_client()
 	end
 end
