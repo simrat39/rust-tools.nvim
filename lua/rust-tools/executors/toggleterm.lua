13e19081bd33ac94b73d87024ff4e7d62c45736e
zmtq05 <zmtq05@gmail.com>
2022-01-16 19:17:27 +0900

    fix(executor): Execute toggleterm with normal mode

--- lua/rust-tools/executors/toggleterm.lua
+++ lua/rust-tools/executors/toggleterm.lua
@@ -16,6 +16,13 @@ function M.execute_command(command, args, cwd)
 			dir = cwd,
 			cmd = utils.make_command_from_args(command, args),
 			close_on_exit = false,
+      on_open = function(t)
+        -- enter normal mode
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes([[<C-\><C-n>]], true, true, true), "", true)
+
+        -- set close keymap
+        vim.api.nvim_buf_set_keymap(t.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
 		})
 		:toggle()
 end
