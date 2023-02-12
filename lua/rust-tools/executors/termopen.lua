8404a8ed8ee1a277a2290b207583e9a167522d52
Ciel <47144701+Ciel-MC@users.noreply.github.com>
2023-02-12 17:12:22 +0800

    Fix :RustRunnable on fish

--- lua/rust-tools/executors/termopen.lua
+++ lua/rust-tools/executors/termopen.lua
@@ -6,7 +6,7 @@ local latest_buf_id = nil
 
 function M.execute_command(command, args, cwd)
   local full_command = utils.chain_commands({
-    utils.make_command_from_args("cd", { cwd }),
+    utils.make_command_from_args("cd", { '"' .. cwd .. '"' }),
     utils.make_command_from_args(command, args),
   })
 
