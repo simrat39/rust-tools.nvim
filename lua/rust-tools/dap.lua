c0c3d65ffb82a8e4544a2da5ac3afa2870ffec2d
Nbiba Bedis <bedisnbiba@gmail.com>
2023-04-16 13:50:43 +0100

    feat(dap): add rust types
    Same trick as rust-lldb to add rust types so Vec, String, ..etc
    are pretty printed in the debugger windows

--- lua/rust-tools/dap.lua
+++ lua/rust-tools/dap.lua
@@ -134,6 +134,26 @@ function M.start(args)
             args = args.executableArgs or {},
             cwd = args.workspaceRoot,
             stopOnEntry = false,
+            -- Add rust types
+            initCommands = function()
+              -- Find out where to look for the pretty printer Python module
+              local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+              local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+              local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+              local commands = {}
+              local file = io.open(commands_file, 'r')
+              if file then
+                for line in file:lines() do
+                  table.insert(commands, line)
+                end
+                file:close()
+              end
+              table.insert(commands, 1, script_import)
+
+              return commands
+            end,
 
             -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
             --
