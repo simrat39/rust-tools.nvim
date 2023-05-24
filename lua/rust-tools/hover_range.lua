669153f8a857c8d99f04ce84424d13ea79b97b90
Ciel <47144701+Shuaiouke@users.noreply.github.com>
2023-05-24 22:48:46 +0800

    fix(hover range): It actually works well now

--- lua/rust-tools/hover_range.lua
+++ lua/rust-tools/hover_range.lua
@@ -2,28 +2,46 @@ local rt = require("rust-tools")
 
 local M = {}
 
+local function pos_to_lsp_pos(vim_pos)
+  return {
+    line = vim_pos[2] - 1,
+    character = vim_pos[3]
+  }
+end
+
 local function get_opts()
-  local params = vim.lsp.util.make_range_params()
-  -- set start and end of selection
-  local start_m = vim.api.nvim_buf_get_mark(0, "<")
-  local end_m = vim.api.nvim_buf_get_mark(0, ">")
-  params.range.start = {
-    character = start_m[2],
-    line = start_m[1] - 1, -- vim starts counting at 1, but lsp at 0
-  }
-  params.range["end"] = {
-    character = end_m[2],
-    line = end_m[1] - 1, -- vim starts counting at 1, but lsp at 0
-  }
-  params.position = params.range
-  params.range = nil
-  --print(vim.inspect(params))
+  local range = vim.lsp.util.make_range_params()
+
+  local start_pos = pos_to_lsp_pos(vim.fn.getpos("v"))
+  local end_pos = pos_to_lsp_pos(vim.fn.getpos("."))
+  -- vim.notify("start_pos: " .. vim.inspect(start_pos), vim.log.levels.INFO)
+  -- vim.notify("end_pos: " .. vim.inspect(end_pos), vim.log.levels.INFO)
+
+  local params = {}
+  params.textDocument = range.textDocument
+  if start_pos == end_pos then
+    params.position = start_pos
+  else
+    params.position = {
+      start = start_pos,
+      ["end"] = end_pos,
+    }
+  end
 
   return params
 end
 
 function M.hover_range()
-  rt.utils.request(0, "textDocument/hover", get_opts())
+  -- rt.utils.request(0, "textDocument/hover", get_opts(), function(...)
+  --   vim.print("Callback:")
+  --   vim.print(...)
+  --   vim.print("End of callback")
+  -- end)
+  -- vim.notify(vim.inspect(get_opts()), vim.log.levels.INFO)
+  rt.utils.request(0, "textDocument/hover", get_opts(), function(...)
+    -- vim.print(...)
+    require("rust-tools.hover_actions").handler(...)
+  end)
 end
 
 return M
