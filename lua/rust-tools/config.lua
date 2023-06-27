ef60eac58632dd94b2a4cbd7655069fca927e140
CPea <cpea2506@gmail.com>
2023-06-27 15:38:11 +0700

    feat(opts): `executor` could be a string

--- lua/rust-tools/config.lua
+++ lua/rust-tools/config.lua
@@ -12,7 +12,7 @@ local defaults = {
 
     -- how to execute terminal commands
     -- options right now: termopen / quickfix / toggleterm / vimux
-    executor = require("rust-tools.executors").termopen,
+    executor = require("rust-tools.executors").termopen,  -- or "termopen"
 
     -- callback to execute once rust-analyzer is done initializing the workspace
     -- The callback receives one parameter indicating the `health` of the server: "ok" | "warning" | "error"
