8b32537618d80b966ffbf1f4ec4d8bd6fa36338a
Yura Taras <yura.taras@gmail.com>
2023-08-29 19:54:51 +0300

    Fixing last_runnable

--- lua/rust-tools/cached_commands.lua
+++ lua/rust-tools/cached_commands.lua
@@ -7,7 +7,7 @@ local cache = {
 }
 
 -- @param action 
-M.set_last_runnable = function(c, r)
+M.set_last_runnable = function(choice, result)
   cache.last_runnable = { choice, result }
 end
 
@@ -28,7 +28,7 @@ end
 M.execute_last_runnable = function()
   local action = cache.last_runnable
   if action then 
-    rt.runnables.run_command(action[0], action[1])
+    rt.runnables.run_command(action[1], action[2])
   else
     rt.runnables.runnables()
   end
